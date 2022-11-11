49cd6c60c0d90a8e656afe45583026aa4a438a8f
cc-21 <huicong.huang21@gmail.com>
2022-11-10 22:40:47 -0800

    Remove unnecessary util functions

--- src/util/FileUtil.py
+++ src/util/FileUtil.py
@@ -1,15 +1,3 @@
-import pandas as pd
-from pandas import DataFrame
-
-
-def get_dataFrame_from_csv(file: str) -> DataFrame:
-    return pd.read_csv(file)
-
-
-def save_to_csv(file: DataFrame, save_file: str) -> None:
-    file.to_csv(save_file, index=False)
-
-
 def getKeywordFromFile(file_path: str) -> str:
     f = open(file_path)
     keywords = f.read().splitlines()
