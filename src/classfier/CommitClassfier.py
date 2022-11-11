89895ea356e43b73122e5164261cf8b0d33ea64f
Noir Tree <2515744793@qq.com>
2022-11-10 23:26:46 -0800

    - add regex for "introduce_issue_PR_reference" and "out_of_date". - fix small bug in `info` so that it can correctly show results of regex   with multiple groups.

--- src/classfier/CommitClassfier.py
+++ src/classfier/CommitClassfier.py
@@ -8,9 +8,9 @@ import re
 from util.FileUtil import getKeywordFromFile
 from project_path import ROOT_DIR
 
-
 TO_FIX_DEFECTS_KEYWORD_FILE = "{}/data/pattern/to_fix_defects_keyword.txt".format(ROOT_DIR)
-
+LINK_ISSUE_VERB_KEYWORD_FILE = "{}/data/pattern/link_issue_verb_keyword.txt".format(ROOT_DIR)
+OUT_OF_DATE_KEYWORD_FILE = "{}/data/pattern/out_of_date_keyword.txt".format(ROOT_DIR)
 
 class CommitCategory(namedtuple('CommitCategory', 'name regex_exp'), Enum):
     """
@@ -18,8 +18,11 @@ class CommitCategory(namedtuple('CommitCategory', 'name regex_exp'), Enum):
     regex_exp is the regular expression of the pattern for the category
     """
     DESCRIBE_ERROR_SCENARIO = "describe_error_scenario", ".*error.*"
-    TO_FIX_DEFECTS = "to_fix_defects", "({})".format(getKeywordFromFile(TO_FIX_DEFECTS_KEYWORD_FILE))
+    TO_FIX_DEFECTS = "to_fix_defects", "({})+".format(getKeywordFromFile(TO_FIX_DEFECTS_KEYWORD_FILE))
     ISSUE_LINK = "issue_link", "https"
+    INTRODUCE_ISSUE_PR_REFERENCE = "introduce_issue_PR_reference", \
+                                   "(?P<link_issue_verb>{})?.*?(issue|review)?.*?(?P<issue_link>\#\d+)+".format(getKeywordFromFile(LINK_ISSUE_VERB_KEYWORD_FILE))
+    OUT_OF_DATE = "out_of_date", "({})+".format(getKeywordFromFile(OUT_OF_DATE_KEYWORD_FILE))
 
     def __str__(self) -> str:
         return self.name
@@ -40,7 +43,7 @@ def pattern_match(pattern: CommitCategory, commit_msg: str, verbose: bool = Fals
 
 def info(pattern_name: str, commit_msg: str, all_found_patterns: list):
     print("Commit Message: {}, Pattern: {}\nFound patterns: {}\n".format(commit_msg, pattern_name,
-                                                                         ", ".join(all_found_patterns)))
+                                                                         all_found_patterns))
 
 
 def commit_classifier(commit_msg: str) -> list:
@@ -61,7 +64,7 @@ if __name__ == "__main__":
     """
     pipeline for classifying a single commit message
     """
-    commit_msg_test: str = "fix error feat:"
+    commit_msg_test: str = "actually deprecate stuff"
 
     # preprocess commit msg
     commit_msg_test = preprocess(commit_msg_test)
