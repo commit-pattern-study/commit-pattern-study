7f02ed11ef5c858bb42bb5055b076313c1eca1ce
cc-21 <huicong.huang21@gmail.com>
2022-11-11 02:47:41 -0800

    Encapsulate the classifier-related functions

--- src/classfier/CommitClassfier.py
+++ src/classfier/CommitClassfier.py
@@ -12,6 +12,7 @@ TO_FIX_DEFECTS_KEYWORD_FILE = "{}/data/pattern/to_fix_defects_keyword.txt".forma
 LINK_ISSUE_VERB_KEYWORD_FILE = "{}/data/pattern/link_issue_verb_keyword.txt".format(ROOT_DIR)
 OUT_OF_DATE_KEYWORD_FILE = "{}/data/pattern/out_of_date_keyword.txt".format(ROOT_DIR)
 
+
 class CommitCategory(namedtuple('CommitCategory', 'name regex_exp'), Enum):
     """
     Enum class for each category, in which name attribute (the first one) is the unique name for the category. And the
@@ -21,52 +22,57 @@ class CommitCategory(namedtuple('CommitCategory', 'name regex_exp'), Enum):
     TO_FIX_DEFECTS = "to_fix_defects", "({})+".format(getKeywordFromFile(TO_FIX_DEFECTS_KEYWORD_FILE))
     ISSUE_LINK = "issue_link", "https"
     INTRODUCE_ISSUE_PR_REFERENCE = "introduce_issue_PR_reference", \
-                                   "(?P<link_issue_verb>{})?.*?(issue|review)?.*?(?P<issue_link>\#\d+)+".format(getKeywordFromFile(LINK_ISSUE_VERB_KEYWORD_FILE))
+                                   "(?P<link_issue_verb>{})?.*?(issue|review)?.*?(?P<issue_link>\#\d+)+".format(
+                                       getKeywordFromFile(LINK_ISSUE_VERB_KEYWORD_FILE))
     OUT_OF_DATE = "out_of_date", "({})+".format(getKeywordFromFile(OUT_OF_DATE_KEYWORD_FILE))
 
     def __str__(self) -> str:
         return self.name
 
 
-class CommitClassifier():
-    def __init__(self, commit_msg: str):
+class CommitClassifier:
+    def __init__(self, commit_msgs: list[str], verbose: bool = False):
         """
             return list of categories for commit_msg
             """
 
-        self.res: list = []
-        self.commit_msg = commit_msg
+        self.commit_category_dict = {}
+        self.commit_msgs = commit_msgs
+        self.preprocessed_msgs = []
+        self.verbose = verbose
 
     def preprocess(self):
         """
         preprocess commit_msg
         """
-        tokenized_lst = stem_tokenize(self.commit_msg)
-        self.commit_msg = ' '.join(tokenized_lst)
-        self.commit_msg = remove_stop_words(self.commit_msg)
-        self.commit_msg = denoise(self.commit_msg)
+        for commit_msg in self.commit_msgs:
+            tokenized_lst = stem_tokenize(commit_msg)
+            tokenized_msg = ' '.join(tokenized_lst)
+            tokenized_msg = remove_stop_words(tokenized_msg)
+            tokenized_msg = denoise(tokenized_msg)
+            self.preprocessed_msgs.append(tokenized_msg)
+            if self.verbose:
+                print('raw string: {}, preprocessed string: {}\n'.format(commit_msg, tokenized_msg))
 
-    def pattern_match(self, pattern: CommitCategory, verbose: bool = False) -> bool:
-        """
-        return true if commit_msg matched pattern
-        """
-        if re.search(pattern.regex_exp, self.commit_msg):
-            if verbose:
-                self.info(pattern.name, re.findall(pattern.regex_exp, self.commit_msg))
-            return True
-
-        return False
-
-    def classify(self) -> list:
+    def classify(self):
         self.preprocess()
-        for pattern in CommitCategory:
-            if self.pattern_match(pattern, True):
-                self.res.append(pattern)
-        return self.res
+        for commit_msg in self.commit_msgs:
+            commit_categories = []
+            matched_substrings = []
+            for pattern in CommitCategory:
+                if re.search(pattern.regex_exp, commit_msg):
+                    if self.verbose:
+                        matched_substrings.append(re.findall(pattern.regex_exp, commit_msg))
+                    commit_categories.append(pattern)
+            if self.verbose:
+                print("Commit Message: {}, Categories: {}\n Matched substrings: {}\n".format(commit_msg, commit_categories,
+                                                                                        matched_substrings))
+            self.commit_category_dict[commit_msg] = commit_categories
 
-    def info(self, pattern_name: str, all_found_patterns: list):
-        print("Commit Message: {},Category: {}\nMatched patterns: {}\n".format(self.commit_msg, pattern_name,
-                                                                             ", ".join(all_found_patterns)))
+    def pretty_print(self):
+        for commit_msg in self.commit_msgs:
+            categories = self.commit_category_dict[commit_msg]
+            print("Commit Message: {}, Categories: {}".format(commit_msg, ", ".join([str(e) for e in categories])))
 
 
 if __name__ == "__main__":
@@ -74,10 +80,9 @@ if __name__ == "__main__":
     pipeline for classifying a single commit message
     """
     commit_msg_test: str = "fixed error feat:"
-    commit_classifer = CommitClassifier(commit_msg_test)
-    commit_classifer.preprocess()
-    print('raw string: {}, preprocessed string: {}'.format(commit_msg_test, commit_classifer.commit_msg))
+    commit_msg_test2: str = "actually deprecate stuff"
+    commit_classifer = CommitClassifier([commit_msg_test, commit_msg_test2], True)
+    commit_classifer.classify()
+    commit_classifer.pretty_print()
 
-    # get the list of Patterns that the commit message can be classified to
-    category: list = commit_classifer.classify()
-    print("Commit Message: {} belongs to {} categories".format(commit_msg_test, ", ".join([str(e) for e in category])))
+
