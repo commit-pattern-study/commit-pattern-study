08729a360779b7d7ad37574e6ba613166c2f5458
cc-21 <huicong.huang21@gmail.com>
2022-11-20 23:43:04 -0800

    Add binary layer evaluation

--- README.md
+++ README.md
@@ -4,3 +4,7 @@
 - `data/finalized` contains commit messages that have been labelled by three authors and processed such that only the final categorization is preserved
 - `data/category` contains commit messages that have been labelled by three authors, partitioned by category
 - `data/intercoder` contains the codes of labelled commit messages for inter-coder agreement calculation
+
+Reference
+
+https://kawahara.ca/how-to-compute-truefalse-positives-and-truefalse-negatives-in-python-for-binary-classification-problems/
--- src/evaluation/ClassiferEvaluation.py
+++ src/evaluation/ClassiferEvaluation.py
@@ -1,8 +1,9 @@
 import os
-import pandas as pd
 import ast
+import pandas as pd
+import numpy as np
 
-from classfier.CommitClassfier import CommitClassifier
+from classfier.CommitClassfier import CommitClassifier, WhyCategory, WhatCategory
 
 categories = ['why_category', 'why_subcategory', 'what_category', 'what_subcategory']
 
@@ -40,6 +41,12 @@ def get_train_test_set(train_file, test_file):
     for col in categories:
         train_df[col] = train_df[col].apply(lambda x: ast.literal_eval(x))
         test_df[col] = test_df[col].apply(lambda x: ast.literal_eval(x))
+    train_df['good'] = train_df.apply(lambda x: len(x.why_subcategory) != 0 and len(x.what_subcategory) != 0
+                                                and WhyCategory.MISSING.name not in x.why_subcategory
+                                                and WhatCategory.MISSING.name not in x.what_subcategory, axis=1)
+    test_df['good'] = test_df.apply(lambda x: len(x.why_subcategory) != 0 and len(x.what_subcategory) != 0
+                                              and WhyCategory.MISSING.name not in x.why_subcategory
+                                              and WhatCategory.MISSING.name not in x.what_subcategory, axis=1)
     print('train set size: {}, test set size: {}'.format(train_df.shape, test_df.shape))
     return train_df, test_df
 
@@ -62,13 +69,38 @@ class ClassifierEvaluation:
         self.test_results = self.test_classifer.get_results()
 
 
-def get_acc(results, ground_truth):
+def get_why_acc(results, ground_truth):
     acc = []
-    for true_category, classified_category in zip(ground_truth['why_subcategory'],
-                                                  results['why_subcategory']):
+    for true_category, classified_category in zip(ground_truth['why_subcategory'], results['why_subcategory']):
         acc.append(any(item in classified_category for item in true_category))
     print('# of correctly labelled: {}, total: {}, acc: {}'.format(sum(acc), len(acc), sum(acc) / len(acc)))
     return sum(acc) / len(acc)
+
+
+def get_what_acc(results, ground_truth):
+    acc = []
+    for true_category, classified_category in zip(ground_truth['what_subcategory'], results['what_subcategory']):
+        acc.append(any(item in classified_category for item in true_category))
+    print('# of correctly labelled: {}, total: {}, acc: {}'.format(sum(acc), len(acc), sum(acc) / len(acc)))
+    return sum(acc) / len(acc)
+
+
+def get_binary_evaluation(true_labels, pred_labels):
+    # True Positive (TP): we predict a label of 1 (positive), and the true label is 1.
+    TP = np.sum(np.logical_and(pred_labels == 1, true_labels == 1))
+    # True Negative (TN): we predict a label of 0 (negative), and the true label is 0.
+    TN = np.sum(np.logical_and(pred_labels == 0, true_labels == 0))
+    # False Positive (FP): we predict a label of 1 (positive), but the true label is 0.
+    FP = np.sum(np.logical_and(pred_labels == 1, true_labels == 0))
+    # False Negative (FN): we predict a label of 0 (negative), but the true label is 1.
+    FN = np.sum(np.logical_and(pred_labels == 0, true_labels == 1))
+    print('TP: %i, FP: %i, TN: %i, FN: %i' % (TP, FP, TN, FN))
+
+    acc = (TP + TN) / (TP + TN + FP + FN)
+    precision = TP / (TP + FP)
+    recall = TP / (TP + FN)
+    f1 = 2 * (precision * recall / (precision + recall))
+    print('Binary Classification - Accuracy: {}, Precision: {}, Recall: {}, F1:{}'.format(acc, precision, recall, f1))
 
 
 if __name__ == '__main__':
@@ -77,5 +109,10 @@ if __name__ == '__main__':
     train_df, test_df = get_train_test_set('train_set.xlsx', 'test_set.xlsx')
     classifer_evaluation = ClassifierEvaluation(train_df, test_df)
     classifer_evaluation.classify()
-    acc_train = get_acc(classifer_evaluation.train_results, classifer_evaluation.train_data)
-    acc_test = get_acc(classifer_evaluation.test_results, classifer_evaluation.test_data)
+    acc_train = get_why_acc(classifer_evaluation.train_results, classifer_evaluation.train_data)
+    acc_test = get_why_acc(classifer_evaluation.test_results, classifer_evaluation.test_data)
+    acc_what_train = get_what_acc(classifer_evaluation.train_results, classifer_evaluation.train_data)
+    acc_what_test = get_what_acc(classifer_evaluation.test_results, classifer_evaluation.test_data)
+
+    get_binary_evaluation(train_df['good'], classifer_evaluation.train_results['good_classified'])
+    get_binary_evaluation(test_df['good'], classifer_evaluation.test_results['good_classified'])
